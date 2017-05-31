cd371603d5a85e3bc048b12b83a53755c956668e
Ruslan <ruslan.yatsevich@gmail.com>
2017-05-31 19:18:52 +0300

    project structure & gulp build

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
+var gsi = require('gulp-scripts-index');
+// var gsti = require('./custom_modules/gulp-styles-index');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+var htmlreplace = require('gulp-html-replace');
+var rename = require('gulp-rename');
+var add = require('gulp-add-src');
+var connect = require('gulp-connect');
+// var history = require('connect-history-api-fallback');
+
+
+gulp.task('clean', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('scss', function () {
+    return gulp.src('src/scss/global.scss')
+        // .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch.scss', function () {
+    gulp.watch(['src/scss/*.scss'], ['scss']);
+});
+
+gulp.task('minify.images', function() {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/images'))
+});
+
+
+gulp.task('minify.js', function () {
+    gulp.src(['src/index.html'])
+        .pipe(gsi())
+        .pipe(uglify({mangle: false}))
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('build/js'));
+
+    // return gulp.src(['src/**/*.map'])
+    //     .pipe(rename({dirname: ''}))
+    //     .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('minify.css', ['scss'], function() {
+    return gulp.src(['src/css/*.css', 'src/bower_components/bootstrap/dist/css/bootstrap.min.css'])
+        .pipe(cleanCSS())
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('change.index', ['minify.images', 'minify.js', 'minify.css'], function() {
+    return gulp.src('src/index.html')
+        .pipe(htmlreplace({
+            js: 'js/all.js',
+            css: 'css/all.css'
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('devServer', ['scss', 'watch.scss'], function() {
+    // var middleware = history({});
+    connect.server({
+        root: 'src',
+        livereload: true,
+        port: 8000
+        // middleware: function (connect, opt) {
+        //     return [middleware];
+        // }
+    });
+});
+
+gulp.task('prodServer', ['change.index'],
+    function() {
+        connect.server({
+            root: 'build',
+            livereload: true,
+            port: 9000
+        });
+    }
+);
+
+
+gulp.task('build', ['clean'], function () {
+    gulp.run('prodServer')
+});
+
+gulp.task('default', ['devServer']);
